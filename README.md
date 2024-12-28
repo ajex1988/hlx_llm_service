8aee0885f1b9445f72c183b933d10b2dd6d804a2
zhe.zhu <zhe.zhu@samsung.com>
2024-12-28 11:31:32 -0800

    First workable version

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ conda activate llm_service
 ```
 Install the PyTorch according to the CUDA version. For example, 
 ```angular2html
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 ```
 Install the required libraries
 ```angular2html
--- qa_demo.py
+++ qa_demo.py
@@ -14,6 +14,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, default='./', help='Model Directory')
     parser.add_argument('--port', type=int, default=3096)
+    parser.add_argument('--max_new_tokens', type=int, default=5000)
     parser.add_argument('--max_concurrent', type=int, default=5)
 
     args = parser.parse_args()
@@ -25,11 +26,13 @@ if __name__ == '__main__':
     model_path = args.model_path
     MAX_CONCURRENT_REQUESTS = args.max_concurrent
     port = args.port
+    max_new_tokens = args.max_new_tokens
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(device)
 
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto', trust_remote_code=True)
 
     lock = threading.Lock()
     counter = 0
@@ -50,16 +53,36 @@ if __name__ == '__main__':
 
         try:
             question = request.json['question']
-            question += "->"
-            inputs = tokenizer(question, return_tensors='pt')
-            inputs = inputs.to(device)
-            pred = model.generate(**inputs)
+            # question += "->"
+            # inputs = tokenizer(question, return_tensors='pt')
+            # inputs = inputs.to(device)
+            # pred = model.generate(**inputs, max_new_tokens=20000)
+            #
+            # text = tokenizer.decode(pred.cpu().detach().numpy()[0])
+            messages = [
+                {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+                {"role": "user", "content": question}
+            ]
+            text = tokenizer.apply_chat_template(
+                messages,
+                tokenize=False,
+                add_generation_prompt=True
+            )
+            model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-            text = tokenizer.decode(pred.cpu().detach().numpy()[0])
+            generated_ids = model.generate(
+                **model_inputs,
+                max_new_tokens=max_new_tokens
+            )
+            generated_ids = [
+                output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+            ]
+
+            text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
             print(f"prediction: {text}")
 
-            response = {'answer': text[len(question):]}
+            response = {'answer': text}
             return jsonify(response)
         finally:
             with lock:
